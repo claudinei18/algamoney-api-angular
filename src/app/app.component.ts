274647fc083144b3894f0b6cb7db058c2eae6747
Claudinei <claudineigom18@gmail.com>
2017-09-19 11:54:20 -0300

    12.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cliente: Cliente = new Cliente();
 
+  nome;
+  profissoes = [
+    {id: 1, nome: 'Programador'},
+    {id: 2, nome: 'Empresário'},
+    {id: 3, nome: 'Outra'}
+  ];
+  profissao = 3;
+  salvar(usuario: NgForm) {
+    console.log(usuario.value);
+    console.log(this.cliente);
+  }
 }
